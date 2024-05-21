aaaa4d2059e6e70e321a8ee5b7d5b4dd5def846c
Aaron Coburn <aaronc@inrupt.com>
2024-05-21 13:41:08 -0500

    More linting

--- pages/api/app.ts
+++ pages/api/app.ts
@@ -28,15 +28,15 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     return res.status(405).send("Method Not Allowed");
   }
 
-  let hostname;
+  let domain;
   if (process.env.VERCEL_ENV === "production") {
-      hostname = process.env.PROD_DOMAIN;
+    domain = process.env.PROD_DOMAIN;
   } else {
-      hostname = process.env.VERCEL_BRANCH_URL;
+    domain = process.env.VERCEL_BRANCH_URL;
   }
 
-  const clientId = new URL("/api/app", `https://${hostname}`);
-  const hostname = new URL("/", `https://${hostname}`);
+  const clientId = new URL("/api/app", `https://${domain}`);
+  const hostname = new URL("/", `https://${domain}`);
 
   const acceptedType = accepts(req).type([
     "application/ld+json",
